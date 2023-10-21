9dd18bd64cfe415165a106e30fb14e5c54998911
Vadym Ladimyrych <vadym.ladimyrych@whitebox-services.eu>
2023-10-21 18:38:20 +0200

    Update UI

--- components/LotteryEntrance.js
+++ components/LotteryEntrance.js
@@ -93,7 +93,7 @@ export default function LotteryEntrance() {
         const lotteryPrizeFromCall = (await getLotteryPrize()).toString();
         const playerAmountFromCall = (await getGetPlayerAmount()).toString();
         const lotteryStateFromCall = (await getLotteryState()).toString();
-        // const playersAmountAvgFromCall = (await getPlayersAmountAvg()).toString();
+        const playersAmountAvgFromCall = (await getPlayersAmountAvg()).toString();
 
         setEntranceAmount(minEntrancePriceFromCall);
         setMinEntrancePrice(minEntrancePriceFromCall);
@@ -102,7 +102,7 @@ export default function LotteryEntrance() {
         setLotteryPrize(lotteryPrizeFromCall);
         setPlayerAmount(playerAmountFromCall);
         setLotteryState(lotteryStateFromCall);
-        // setPlayersAmountAvg(playersAmountAvgFromCall);
+        setPlayersAmountAvg(playersAmountAvgFromCall);
     }
 
     useEffect(() => {
@@ -157,7 +157,7 @@ export default function LotteryEntrance() {
         dispatch({
             type: "info",
             message: "You have successfully entered lottery!",
-            title: "Entrance confirmed",
+            title: "Lottery entered",
             position: "topR",
             icon: "bell",
         });
@@ -167,7 +167,7 @@ export default function LotteryEntrance() {
         dispatch({
             type: "success",
             message: `Winner address: ${winner}`,
-            title: "The winner has been found!",
+            title: "The winner has been selected!",
             position: "topR",
             icon: "bell",
         });
@@ -177,7 +177,7 @@ export default function LotteryEntrance() {
         dispatch({
             type: "warning",
             message: "The current lottery round has ended, a winner is being sought.",
-            title: "Lottery round ended ...",
+            title: "Lottery round ended",
             position: "topR",
             icon: "bell",
         });
@@ -209,11 +209,24 @@ export default function LotteryEntrance() {
         <div className="p-5">
             {lotteryAddress ? (
                 <>
-                    <div className="h-56 grid grid-cols-5 gap-4 content-center">
+                    <div className="h-70 grid grid-cols-7 gap-4 content-center">
+                        <div className="col-start-3 col-span-3">
+                            { lotteryState == "0" ? (
+                                <div className="p-1 text-center border rounded-lg box-decoration-slice bg-gradient-to-r from-green-500 to-green-700 text-white px-2 text-3xl">
+                                    LOTTERY IS OPEN
+                                </div>
+                            ) : (
+                                <div className="p-1 text-center border rounded-lg box-decoration-slice bg-gradient-to-r from-yellow-600 to-red-600 text-white px-2 text-3xl">
+                                    WINNER CALCULATING
+                                </div>
+                            ) }
+                        </div>
+                    </div>
+                    <div className="h-70 grid grid-cols-5 gap-4 content-center">
                         <div className="col-start-3 col-span-1">
-                            <div class="relative mt-2 rounded-md shadow-sm mb-3">
-                                <div class="pointer-events-none absolute inset-y-0 left-0 flex items-center pl-3">
-                                    <svg class="w-4 h-4 mr-2 -ml-1 text-[#626890]" aria-hidden="true" focusable="false" data-prefix="fab" data-icon="ethereum" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 320 512"><path fill="currentColor" d="M311.9 260.8L160 353.6 8 260.8 160 0l151.9 260.8zM160 383.4L8 290.6 160 512l152-221.4-152 92.8z"></path></svg>
+                            <div className="relative mt-2 rounded-md shadow-sm">
+                                <div className="pointer-events-none absolute inset-y-0 left-0 flex items-center pl-3">
+                                    <svg className="w-4 h-4 mr-2 -ml-1 text-[#626890]" aria-hidden="true" focusable="false" data-prefix="fab" data-icon="ethereum" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 320 512"><path fill="currentColor" d="M311.9 260.8L160 353.6 8 260.8 160 0l151.9 260.8zM160 383.4L8 290.6 160 512l152-221.4-152 92.8z"></path></svg>
                                 </div>
                                 <input
                                     type="number"
@@ -225,14 +238,16 @@ export default function LotteryEntrance() {
                                     min={ ethers.utils.formatEther(minEntrancePrice) }
                                     step={ ethers.utils.formatEther(minEntrancePrice) }
                                 />
-                                <div class="pointer-events-none absolute inset-y-0 right-0 flex items-center pr-3">
-                                    <span class="text-gray-500 sm:text-sm" id="price-currency">ETH</span>
+                                <div className="pointer-events-none absolute inset-y-0 right-0 flex items-center pr-3">
+                                    <span className="text-gray-500 sm:text-sm" id="price-currency">ETH</span>
                                 </div>
                             </div>
-
+                            <div className="text-gray-500 sm:text-sm text-center">
+                                Min Entrance Price: { ethers.utils.formatEther(minEntrancePrice) } ETH
+                            </div>
                             <button
                                 type="button"
-                                className="w-full text-lg text-white bg-gradient-to-r from-cyan-500 to-blue-500 hover:bg-gradient-to-bl focus:ring-4 focus:outline-none focus:ring-cyan-300 dark:focus:ring-cyan-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center mr-2 mb-2"
+                                className="w-full text-lg text-white bg-gradient-to-r from-cyan-500 to-blue-500 hover:bg-gradient-to-bl focus:ring-4 focus:outline-none focus:ring-cyan-300 dark:focus:ring-cyan-800 font-medium rounded-lg px-5 py-2.5 text-center mr-2 mb-2"
                                 onClick={ handleSubmit }
                                 disabled={ isLoading || isFetching }
                             >
@@ -243,18 +258,70 @@ export default function LotteryEntrance() {
                                 ) }
                             </button>
                         </div>
+                        <div className="col-start-2 col-span-3">
+                            <div className="relative overflow-x-auto">
+                                <div className="border rounded-lg overflow-hidden">
+                                    <table className="w-full text-sm text-left text-gray-500 dark:text-gray-400">
+                                        <tbody>
+                                            <tr className="bg-white border-b dark:bg-gray-800 dark:border-gray-700">
+                                                <th scope="row" className="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
+                                                    Jackpot
+                                                </th>
+                                                <td className="px-6 py-4">
+                                                    { ethers.utils.formatEther(lotteryPrize) } ETH
+                                                </td>
+                                            </tr>
+                                            <tr className="bg-white border-b dark:bg-gray-800 dark:border-gray-700">
+                                                <th scope="row" className="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
+                                                    Number of players
+                                                </th>
+                                                <td className="px-6 py-4">
+                                                    { playersCount }
+                                                </td>
+                                            </tr>
+                                            <tr className="bg-white border-b dark:bg-gray-800 dark:border-gray-700">
+                                                <th scope="row" className="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
+                                                    Average bid
+                                                </th>
+                                                <td className="px-6 py-4">
+                                                    { ethers.utils.formatEther(playersAmountAvg) } ETH
+                                                </td>
+                                            </tr>
+                                            <tr className="bg-white border-b dark:bg-gray-800 dark:border-gray-700">
+                                                <th scope="row" className="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
+                                                    Your bid
+                                                </th>
+                                                <td className="px-6 py-4">
+                                                    { ethers.utils.formatEther(playerAmount) } ETH
+                                                </td>
+                                            </tr>
+                                            <tr className="bg-white dark:bg-gray-800">
+                                                <th scope="row" className="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
+                                                    Your probability of winning
+                                                </th>
+                                                <td className="px-6 py-4">
+                                                    { playerAmount > 0 ? `${(lotteryPrize / playerAmount) * 100}%` : "N/A" }
+                                                </td>
+                                            </tr>
+                                        </tbody>
+                                    </table>
+                                </div>
+                            </div>
+                        </div>
                     </div>
-
-                    <div>Lottery State: { lotteryState == "0" ? "OPEN" : "WINNER CALCULATING" }</div>
-                    <div>Min Entrance Price: { ethers.utils.formatEther(minEntrancePrice) } ETH</div>
-                    <div>Current Lottery user amount: { ethers.utils.formatEther(playerAmount) } ETH</div>
-                    <div>Current Lottery Prize: { ethers.utils.formatEther(lotteryPrize) } ETH</div>
-                    <div>Current Lottery AVG user amount: { ethers.utils.formatEther(playersAmountAvg) } ETH</div>
-                    <div>Current Lottery number of players: { playersCount }</div>
-                    <div>The most previous winner was: { recentWinner } { recentWinner.toLowerCase() == account ? "(you)" : "" }</div>
+                    <div className="h-70 grid grid-cols-7 gap-4 content-center">
+                        <div className="col-start-3 col-span-3">
+                            <div className="p-1 text-center text-md">
+                                Previous winner:
+                            </div>
+                            <div className="p-1 text-center border rounded-lg box-decoration-slice bg-gradient-to-b from-orange-500 to-yellow-300 text-gray-40 px-2 text-md">
+                                { recentWinner }
+                            </div>
+                        </div>
+                    </div>
                 </>
             ) : (
-                <div>Please connect to a supported chain </div>
+                <div>Please connect to a supported chain (Sepolia)</div>
             )}
         </div>
     );
