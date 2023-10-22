becf48bed352d7e6db7de67df5497ae7583420c0
Vadym Ladimyrych <vadym.ladimyrych@whitebox-services.eu>
2023-10-23 00:06:28 +0200

    Add recent winner truncating

--- components/PreviousWinner.js
+++ components/PreviousWinner.js
@@ -4,9 +4,9 @@ export default function PreviousWinner(props) {
             <div className="p-1 text-center text-md">
                 Previous winner:
             </div>
-            <div className="p-1 text-center border rounded-lg box-decoration-slice bg-gradient-to-b from-orange-500 to-yellow-300 text-gray-40 px-2 text-md">
+            <p className="text-ellipsis overflow-hidden p-1 text-center border rounded-lg box-decoration-slice bg-gradient-to-b from-orange-500 to-yellow-300 text-gray-40 px-2 text-md">
                 { props.recentWinner }
-            </div>
+            </p>
         </div>
     );
 }
